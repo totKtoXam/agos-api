ac6fea4d6e242f3e1a78631b2d3e91c72df5d29c
Khamitov Beibarys <kham.b.13.09@gmail.com>
2020-02-16 00:55:46 +0600

    Removed language constants and moved to database in table which called is Languages

--- Constants/Interpreter/LanguageCodeConstants.cs
+++
@@ -1,7 +0,0 @@
-namespace agos_api.Constants.Interpreter
-{
-    public class LanguageCodeConstants
-    {
-        public static readonly string[] LanguageCode = new string [] { "kz", "ru", "en"};
-    }
-}
